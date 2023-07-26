59aa596ca590305d315e8d86d6934b2c9e2a69e5
sean_bellom <bellomsean@gmail.com>
2023-07-26 10:23:46 +0100

    first init

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import Navbar from '../components/navbar'
+export default function Home() {
+  return (
+      <>
+        <Navbar />
+        <div>
+          <h1 className="text-3xl font-bold underline">
+              Hello world!
+          </h1>
+        </div>
+      </>
+  )
+}
